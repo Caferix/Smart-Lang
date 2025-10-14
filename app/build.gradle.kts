21eb9fd882d5b3bab48989ac2180ecf46139c5e0
Cafer Ceviz <caferixx@hotmail.com>
2025-10-14 16:03:26 +0300

    chore: initial project setup add readme and .gitignore, add skeleton folders with .gitkeep

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+import org.gradle.kotlin.dsl.annotationProcessor
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "com.scu.smartlang"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.scu.smartlang"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    // Hilt
+    implementation (libs.hilt.android)
+    annotationProcessor (libs.hilt.android.compiler)
+
+    // Hilt support for viewmodel
+    implementation (libs.hilt.lifecycle.viewmodel)
+    annotationProcessor (libs.hilt.compiler)
+
+    //Firebase
+    implementation(libs.firebase.auth)
+}
