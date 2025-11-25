30ebf7ff7d5a87c25497c06bcd35247493497c21
Cafer Ceviz <caferixx@hotmail.com>
2025-11-25 16:52:09 +0300

    feat: redesign other user profile layout with new UI components

--- app/src/main/java/com/scu/smartlang/presentation/ui/profile/OtherUserFragment.java
+++ app/src/main/java/com/scu/smartlang/presentation/ui/profile/OtherUserFragment.java
@@ -37,7 +37,7 @@ public class OtherUserFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_other_user, container, false);
     }
 
     @Override
